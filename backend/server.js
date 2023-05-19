321de250b7c782748b47670a9a8bcf8c58020b65
yaiza <ytendillo07@gmail.com>
2023-05-19 12:20:26 +0200

    Funcionalidades implementadas

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,54 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Get books by languages
+app.get('/getBooksByLanguage', async (req, res) => {
+  const language = req.query.language
 
-app.get('/libros', async (req, res) => {
-
-  const url = 'https://gutendex.com/books?languages=es'
-
+  const url = `https://gutendex.com/books/?languages=${language}`
   const libros = await fetch(url)
-	.then(res => res.json())
+  .then(res => res.json())
 
-  console.log(libros.results[0])
-  
-  res.send(libros)
-  
+  res.send(libros.results)
   })
-  
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/getBooksBySubject', async (req, res) => {
+  const subject = req.query.subject
+
+  const url = `https://gutendex.com/books/?subject=${subject}`
+  const libros = await fetch(url)
+  .then(res => res.json())
+
+  res.send(libros.results)
+  })
+
+  //Get book by Id
+  app.get('/getBookById', async (req, res) => {
+    const id = req.query.id
+
+    const url = `https://gutendex.com/books/?ids=${id}`
+    const libro = await fetch(url)
+    .then(res => res.json())
+  
+    res.send(libro.results)
+    })
+
+  //Get all books
+  app.get('/getAllBooks', async (req, res) => {
+    const url = 'https://gutendex.com/books'
+    const libros = await fetch(url)
+    .then(res => res.json())
+
+    res.send(libros.results)
+    
+    })
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
